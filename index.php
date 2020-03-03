843bf7592f2c094e76b99c014629bb1b6cbac616
Anirudh Giran <59750165+anirudhgiran@users.noreply.github.com>
2020-03-03 14:52:04 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hello Fred<br>";
+$greet = "Hello Barney<br>";
+
+echo "$greet";
+echo "" .$greet;
+
+$word = "Hello<br>";
+
+if(isset($wrd)){
+    echo "Hi<br>";
+}else{
+    echo "Bye<br>";
+}
